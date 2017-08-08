2eea286c11e649ff1f3db9b8add1d31ca4ddbb28
shymmq <szymek.wysocki@gmail.com>
2017-08-09 00:10:14 +0200

    Fix /Token response

--- src/main/kotlin/com/wabadaba/librusmock/MainController.kt
+++ src/main/kotlin/com/wabadaba/librusmock/MainController.kt
@@ -1,5 +1,6 @@
 package com.wabadaba.librusmock
 
+import com.beust.klaxon.JsonObject
 import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.InputStreamResource
 import org.springframework.web.bind.annotation.*
@@ -10,7 +11,11 @@ class MainController {
 
     @RequestMapping(path = arrayOf("/OAuth/Token"), method = arrayOf(RequestMethod.POST))
     fun login(@RequestHeader("Authorization") authorizationHeader: String): String {
-        return authorizationHeader
+        val response = JsonObject()
+        response.put("access_token", "mock_access_token")
+        response.put("refresh_token", "mock_refresh_token")
+        response.put("expires_in", 1864800)
+        return response.toJsonString()
     }
 
     @RequestMapping(path = arrayOf("/2.0/{endpoint}"),
